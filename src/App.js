b43dee7f64cdbffc7b6f0914ebcc191406bed3b3
Wilmar Alexander Quintero Montoya <111884757+wilq0836@users.noreply.github.com>
2022-11-24 20:03:54 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import App from './App.css';
+
+export function Navegador () {
+  const marcaBicicleta =["scott","colnago","giant","pinarello","trek"];
+  return <div> 
+     <h1 id = "titulo">BIKESTWOGO</h1>  
+     <div id="contenedor">
+  <header>       
+    <nav id = "navegador">
+      <a>Inicio</a>
+      <a>Bicicletas</a>
+      <a>Componentes</a>
+      <a>Accesorios</a>
+      <a>Portafolio</a>
+      <a>Servicios</a>
+      <a>Contacto</a>
+    </nav>
+  </header>
+  </div>
+  </div>
+  
+  };
+       
+
