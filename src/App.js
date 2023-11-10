cfa17abf556cdc264efaaa67a4c9c8ecf8a43924
Wilmar Alexander Quintero Montoya <111884757+wilq0836@users.noreply.github.com>
2023-11-09 23:25:51 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './Componentes/ComponentesApp/Nabvar';
+import Mapas from './Componentes/Mapas/Mapas';
+import { CartProvaider } from './Componentes/Context/CarContext';
+import ConstructorProductos from './Componentes/ConstructorArticulos/Constructor';
+import { VerItem } from './Componentes/PedirItemPorId/VerItem';
+import ItemLisContainer from './Componentes/ComponentesPromise/ItemListContainer';
+import { VerItemFetch } from './Componentes/ComponentesPromise/VerItemId';
+import { VerItemMarcaFireBase } from './Componentes/FireBase/PedirMarcaFireBase';
+import { VerItemIdFireBase } from './Componentes/FireBase/VerIdFirebase';
+import { Formulario } from './Componentes/ComponentesFormulario/Formulario';
+import { CheckOut } from './Componentes/FireBase/CheckOut';
+import CarWidget from './Componentes/CarCompras/CarWidget';
+import { BuscarPedidoFire } from './Componentes/ImpresionFinal/BuscarPedidoFire';
+import { Pedidos } from './Componentes/ImpresionFinal/BuscarPedido';
+
+
+function App() {
+  return (
+
+    
+    <CartProvaider>
+    <BrowserRouter>
+    <div className="App">
+      
+      <h1 className='titulo'>BIKESTWOGO</h1>
+      <Navbar></Navbar>
+      
+      <Routes>
+        <Route path='/'></Route>
+        <Route path='/VerItem/Categorias/:marca' element={<ConstructorProductos propiedad={"Biblioteca"}></ConstructorProductos>}></Route>
+        <Route path='/VerItem/:id' element={<VerItem/>}></Route> 
+        <Route path='/contenedorFetch/'element = {<ItemLisContainer  ></ItemLisContainer>}></Route>
+        <Route path='/VerItemFetch/:id'element = {<VerItemFetch/>}></Route>
+        <Route path='/VerItemFire/Categorias/:marca' element={<VerItemMarcaFireBase/>}></Route> 
+        <Route path='/VerItemFire/:id' element={<VerItemIdFireBase/>}></Route> 
+        <Route path='/Formulario/' element={<Formulario/>}></Route>
+        <Route path='/CheckOut/' element={<CheckOut/>}></Route>
+        <Route path='/Carrito' element={<CarWidget></CarWidget>}></Route>  
+        <Route path='/BuscarPedido/' element={<BuscarPedidoFire/>}></Route> 
+        <Route path='/BuscarPedido/:id' element={<Pedidos/>}></Route> 
+     
+      </Routes>
+
+      <Mapas/>
+    </div>
+    </BrowserRouter>
+    </CartProvaider>
+  );
+}
+
+export default App;
