fb421cb6c27db7eba036a70f3d8bbb390053a7f8
Wilmar Alexander Quintero Montoya <111884757+wilq0836@users.noreply.github.com>
2023-11-27 20:28:50 -0500

    Add files via upload

--- src/CarCompras/CarWidget.jsx
+++ src/CarCompras/CarWidget.jsx
@@ -24,33 +24,36 @@ import { Link } from "react-router-dom";
 
  return(
          
+
+
+  <div className="container11">
             
        
-        <div className="renglonesCarrito2 " >                 
-        <div className="modal-content">
+        <div className="renglonesCarrito " >                 
+        <div className="modal-content" style={{height:"auto", width:1440}}>
                   <div className="modal-header">
-                      <h5 className="modal-title" id="exampleModalLabel">Carrito</h5>
+                      <h5 className="modal-title" id="exampleModalLabel" style={{fontSize:50, marginLeft:570}} >Carrito</h5>
                       
                        </div>
                       
-                          <div className="">
+                          
                              
   <table className="table">
     <thead>
     <tr>
        <th scope="col">Marca</th>
         <th scope="col">Modelo</th>
-         <th scope="col">Cantidad Carrito</th>
+         <th scope="col">Cantidad</th>
            <th scope="col">Adicionar articulos</th>
               <th scope="col">Precio Unitario</th>
                   <th scope="col">Precio Total</th>
                      <th scope="col">Eliminar</th>
                      </tr>                                           
              </thead>
-                  <tbody  id="items">
+                  <tbody  id="items" >
 
-                  { carrito.map((item)=><tr key={ item.id}>                                               
-                          <td >{item.marca}</td>
+                  { carrito.map((item)=><tr key={ item.id} >                                               
+                          <td>{item.marca}</td>
                             <td>{item.modelo}</td>
                             <td>{item.numero}</td>
                                <td><input  id = {`cantidad-producto-${item.id}`} key = {`cantidad-producto-${item.id}`}   type="number"  min="1" max="10"  onClick={(e)=>{
@@ -71,25 +74,28 @@ import { Link } from "react-router-dom";
                                         </tr>
                                              
                                              )} 
-                                </tbody>                                        
+                                </tbody>
+                                                                       
                             </table> 
-                            <h4><th  >Total Compra: ${precioTotal()}</th> </h4>
-                               <h4><th>Cantidad de articulos: {cantidadCarrito()}</th></h4>
+                            
+                               
                                           
                                                  
          
-         <div className="modal-footer">
-         <button type="button" className="btn btn-secondary">Cerrar</button>
+         <div className="modal-footer" style={{ height:90}}>
+         <h4 style={{marginRight:80}}>Cantidad de articulos: {cantidadCarrito()}</h4>
+         <h4 style={{marginRight:80}}>Total Compra: ${precioTotal()} </h4>
          
-          <Link to={'/CheckOut/'} className="btn btn-outline-success me-2">Terminar compra</Link>
-         <button type="button" className="btn btn-outline-success me-2" onClick={()=>vaciarCarrito()} >Vaciar Carrito</button>
+          <Link to={'/CheckOut/'} className="btn btn-outline-success me-2" style={{marginLeft:40, marginRight:30, width:200, fontSize:20}}>Terminar compra</Link>
+         <button type="button" className="btn btn-outline-success me-2" onClick={()=>vaciarCarrito()} style={{marginLeft:30, width:200,fontSize:20}} >Vaciar Carrito</button>
+         <Link to={"/"} type="button" className="btn btn-secondary" style={{marginLeft:30, fontSize:20}}>Cerrar</Link>
          </div>
      </div>
      </div>      
        </div> 
        
      
-     
+       
      
          )
   
